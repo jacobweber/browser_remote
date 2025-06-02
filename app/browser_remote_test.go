dfba009137ecc188672ae0b8feed67362f8fc244
Jacob Weber <jacob@jacobweber.com>
2025-06-02 16:23:46 -0700

    shared setup

--- app/browser_remote_test.go
+++ app/browser_remote_test.go
@@ -8,54 +8,43 @@ import (
 )
 
 func TestApp(t *testing.T) {
-	t.Run("responds to test", func(t *testing.T) {
-		br := browser_remote_tester.NewBrowserRemoteTester()
-		br.Start()
+	br := browser_remote_tester.NewBrowserRemoteTester()
+	br.Start()
+
+	t.Run("responds to query", func(t *testing.T) {
 		listener := br.ListenForQueryToBrowser("name")
 		postDone, recorder, _ := br.SendRequestToWeb("{\"query\":\"name\"}")
 		msg := <-listener
 		br.SendResponseFromBrowser(msg.Id, "ok", "john")
 		br.AssertResponseFromWeb(postDone, recorder, "{\"status\":\"ok\",\"result\":\"john\"}\n", t)
-		br.Cleanup()
 	})
 
 	t.Run("ignores browser responses with invalid IDs", func(t *testing.T) {
-		br := browser_remote_tester.NewBrowserRemoteTester()
-		br.Start()
 		listener := br.ListenForQueryToBrowser("name")
 		postDone, recorder, _ := br.SendRequestToWeb("{\"query\":\"name\"}")
 		msg := <-listener
 		br.SendResponseFromBrowser("xxx", "ok", "jim")
 		br.SendResponseFromBrowser(msg.Id, "ok", "john")
 		br.AssertResponseFromWeb(postDone, recorder, "{\"status\":\"ok\",\"result\":\"john\"}\n", t)
-		br.Cleanup()
 	})
 
 	t.Run("responds with browser error", func(t *testing.T) {
-		br := browser_remote_tester.NewBrowserRemoteTester()
-		br.Start()
 		listener := br.ListenForQueryToBrowser("name")
 		postDone, recorder, _ := br.SendRequestToWeb("{\"query\":\"name\"}")
 		msg := <-listener
 		br.SendResponseFromBrowser(msg.Id, "error", "")
 		br.AssertResponseFromWeb(postDone, recorder, "{\"status\":\"error\",\"result\":\"\"}\n", t)
-		br.Cleanup()
 	})
 
 	t.Run("responds with timeout error", func(t *testing.T) {
-		br := browser_remote_tester.NewBrowserRemoteTester()
-		br.Start()
 		listener := br.ListenForQueryToBrowser("name")
 		postDone, recorder, timeout := br.SendRequestToWeb("{\"query\":\"name\"}")
 		<-listener
 		timeout.FireTimer()
 		br.AssertResponseFromWeb(postDone, recorder, "{\"status\":\"timeout\",\"result\":null}\n", t)
-		br.Cleanup()
 	})
 
 	t.Run("handles overlapping calls", func(t *testing.T) {
-		br := browser_remote_tester.NewBrowserRemoteTester()
-		br.Start()
 		listener2 := br.ListenForQueryToBrowser("age")
 		listener1 := br.ListenForQueryToBrowser("name")
 		postDone1, recorder1, _ := br.SendRequestToWeb("{\"query\":\"name\"}")
@@ -66,12 +55,9 @@ func TestApp(t *testing.T) {
 		br.SendResponseFromBrowser(msg1.Id, "ok", "john")
 		br.AssertResponseFromWeb(postDone1, recorder1, "{\"status\":\"ok\",\"result\":\"john\"}\n", t)
 		br.AssertResponseFromWeb(postDone2, recorder2, "{\"status\":\"ok\",\"result\":\"31\"}\n", t)
-		br.Cleanup()
 	})
 
 	t.Run("handles parallel calls", func(t *testing.T) {
-		br := browser_remote_tester.NewBrowserRemoteTester()
-		br.Start()
 		var wg sync.WaitGroup
 		for idInt := range 10 {
 			wg.Add(1)
@@ -86,6 +72,7 @@ func TestApp(t *testing.T) {
 			}()
 		}
 		wg.Wait()
-		br.Cleanup()
 	})
+
+	br.Cleanup()
 }
