bb892643a3aa43e1ce1d499c6b058f89d5eef7f5
Jacob Weber <jacob@jacobweber.com>
2025-06-03 11:17:09 -0700

    docs

--- app/internal/shared/shared.go
+++ app/internal/shared/shared.go
@@ -6,25 +6,25 @@ import (
 	"unsafe"
 )
 
-// MessageFromBrowser represents a message from the browser to the native host.
+// Request from the browser to the native host.
 type MessageFromBrowser struct {
 	Id     string `json:"id"`
 	Status string `json:"status"`
 	Result any    `json:"result"`
 }
 
-// MessageToBrowser respresents a response from the native host to the browser.
+// Response from the native host to the browser.
 type MessageToBrowser struct {
 	Id    string `json:"id"`
 	Query string `json:"query"`
 }
 
-// MessageToWebServer represents a message to the web server.
+// Request to the web server.
 type MessageToWebServer struct {
 	Query string `json:"query"`
 }
 
-// MessageFromWebServer respresents a response from the web server.
+// Response from the web server.
 type MessageFromWebServer struct {
 	Status string `json:"status"`
 	Result any    `json:"result"`
